fb215c079d7715190ef8b38816f8563dd319ba60
cardontemka <ptemuulen82@gmail.com>
2023-08-16 15:42:31 +0800

    idk

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+MONGODB_URI = process.env.MONGODB_URI
 
 const connect = async () => {
     try {
         mongoose.set('strictQuery', false);
-        await mongoose.connect('mongodb+srv://ptemuulen82:2micDWEv0qURzCTu@cluster0.v5a8obs.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect(MONGODB_URI);
         console.log('Succesfully connected to mongodb');
     } catch (error) {
         throw new Error(error.message);
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const cors = require("cors");
 const { userRoutes } = require("./routes/userRouter");
 const { chatRoutes } = require("./routes/chatRouter");
 const { authRoutes } = require("./routes/authRouter");
+require('dotenv').config();
 
 const app = express();
 
-const port = 8080;
+const port = process.env.PORT || 8080
 
 app.use(cors());
 app.use(express.json());
